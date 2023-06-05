f883f852a26b2756693ea9143234f2619ecdd7b5
Georgi Jelev <135559820+GeorgiJelev@users.noreply.github.com>
2023-06-05 03:55:31 +0300

    Add files via upload

---
+++ 5.Basic Syntax, Conditional Statements and Loopsвс/pattern.py
@@ -0,0 +1,9 @@
+rows = 5
+for i in range(rows):
+    for j in range(i):
+        print(i, end=" ")
+    print("")
+for i in range(rows , 0 , -1):
+    for j in range(i):
+        print(i, end=" ")
+    print("")
