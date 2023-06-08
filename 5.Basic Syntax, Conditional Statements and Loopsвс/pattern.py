8ed7634fad1de8cfbbeb90231fccc14ce36ba61e
Georgi Jelev <135559820+GeorgiJelev@users.noreply.github.com>
2023-06-09 00:51:18 +0300

    Delete 5.Basic Syntax, Conditional Statements and Loopsвс directory

--- 5.Basic Syntax, Conditional Statements and Loopsвс/pattern.py
+++
@@ -1,9 +0,0 @@
-rows = 5
-for i in range(rows):
-    for j in range(i):
-        print(i, end=" ")
-    print("")
-for i in range(rows , 0 , -1):
-    for j in range(i):
-        print(i, end=" ")
-    print("")
