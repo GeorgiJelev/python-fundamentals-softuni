30903089210ea3018232f16f4e161514b88f950b
Georgi Jelev <135559820+GeorgiJelev@users.noreply.github.com>
2023-07-19 19:40:39 +0300

    Add files via upload

---
+++ Lab Classes and Objects/party.py
@@ -0,0 +1,33 @@
+class Person:
+    def __init__(self, name):
+        self.name = name
+
+
+class Party:
+    def __init__(self):
+        self.people = []
+
+    def invited(self, person):
+        self.people.append(person)
+
+    def name_of_attendees(self):
+        return ", ".join([person.name for person in self.people])
+
+    def number_of_guest(self):
+        return len(self.people)
+
+
+party = Party()
+
+while True:
+    command = input()
+
+    if command == "End":
+        break
+
+    name = command
+    person = Person(name)
+    party.invited(person)
+
+print(f"Going: {party.name_of_attendees()}")
+print(f"Total: {party.number_of_guest()}")
