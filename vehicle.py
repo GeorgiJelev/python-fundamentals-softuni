e84c3680ed2e6adcecccd0fa13d5ae5d2ff176f3
Georgi Jelev <135559820+GeorgiJelev@users.noreply.github.com>
2023-07-19 19:46:46 +0300

    Update vehicle.py

--- vehicle.py
+++ vehicle.py
@@ -25,3 +25,14 @@ class Vehicle:
         if self.owner is not None:
             return f"{self.model} {self.type} is owned by: {self.owner}"
         return f"{self.model} {self.type} is on sale: {self.price}"
+
+
+vehicle_type = "car"
+model = "BMW"
+price = 30000
+vehicle = Vehicle(vehicle_type, model, price)
+print(vehicle.buy(15000, "Peter"))
+print(vehicle.buy(35000, "George"))
+print(vehicle)
+vehicle.sell()
+print(vehicle)
